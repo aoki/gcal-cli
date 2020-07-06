28b77bae380ef553af914a4d8775e14770cecd2a
aoki <ringohub@gmail.com>
2020-07-06 20:51:41 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
   },
-  "parser": 'babel-eslint',
-  "extends": "eslint:recommended",
-  "rules": {
-    "indent": [
-      "error",
+  'parser': 'babel-eslint',
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [
+      'error',
       2,
-      { "SwitchCase": 1 }
+      { 'SwitchCase': 1 }
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "quotes": [
-      "error",
-      "single"
+    'quotes': [
+      'error',
+      'single'
     ],
-    "semi": [
-      "error",
-      "always"
+    'semi': [
+      'error',
+      'always'
     ],
-    "no-console": 0
+    'no-console': 0
   }
 };
